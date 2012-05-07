884fb24e7d00b24a172563da3b3ce42e8b8f8f44
Brandon Titus <bjtitus@gmail.com>
2012-05-07 10:27:29 -0600

    Fixing some major launch bugs

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do  
-  provider :open_id, OpenID::Store::Filesystem.new("#{Rails.root}/tmp"), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  provider :open_id, :store => OpenID::Store::Filesystem.new("#{Rails.root}/tmp"), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
   begin
     OauthProvider.all.each do |p|
       # This hack can be removed after the upgrade to omniauth 2.0
