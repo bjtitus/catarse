90d493206bf760535306302b0f9deefaafa296cc
Brandon Titus <bjtitus@gmail.com>
2012-05-21 14:37:05 -0600

    Adding range slider for rates.
    This uses some surprisingly crappy jquery to filter based on rate. Wanted to use Backbone (which would be SUPER elegant) but that's way too much work.
    
    Seems to work for now. Anything more complex might start to get dicey.
    
    And yes, I know I'm abusing CSS classes.

--- public/javascripts/explore_projects.js
+++ public/javascripts/explore_projects.js
@@ -25,3 +25,23 @@ if($('#explore_projects .selected').length == 0){
 }
 $('#explore_projects .selected').click()
 
+$(".sliderbar").noUiSlider("init", { scale: [0.0, 10.0], startMin: 2, startMax: 8, tracker:
+
+  function(){
+    var lowVal = Math.round($(".sliderbar").noUiSlider("getValue")[0]); // Without options, 'getValue' returns an array.
+    $("#lowValue").text(
+      lowVal
+    );
+    var highVal = Math.round($(".sliderbar").noUiSlider("getValue", {point: "upper"}));
+    $("#highValue").text(
+      highVal
+    );
+    for (var i = 0; i < lowVal; i++)
+      $("." + i).hide();
+    for (var x = lowVal; x <= highVal; x++)
+      $("." + x).show();
+    for(var j = highVal+1; j <= 10; j++)
+      $("." + j).hide();
+  }
+
+});
